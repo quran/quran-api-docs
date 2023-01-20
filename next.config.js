80e8d3c924dfd7bf6dc0a3f67788547265eb09b0
Muhammad Muhajir <muhajirframe@gmail.com>
2023-01-20 13:52:43 +0700

    handle redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/qdc-v4/',
+        permanent: false,
+      },
+    ]
+  },
   async rewrites() {
     return [
       {
