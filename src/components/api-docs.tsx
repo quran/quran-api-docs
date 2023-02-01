b23fb78733e4a6c2ca44acdaa0b23f78586b7d77
Muhammad Muhajir <muhajirframe@gmail.com>
2023-02-01 13:55:48 +0700

    update UI

--- src/components/api-docs.tsx
+++ src/components/api-docs.tsx
@@ -15,13 +15,15 @@ const APIDocs = ({ basePath, apiDescriptionUrl }: Props) => {
 
   return (
     <div style={{ height: '100%' }}>
-      <div className="px-4 py-8 md:p-0 h-full">
+      <div className="py-8 md:p-0 h-full">
         <Navbar basePath={basePath} />
-        <API
-          layout={layout}
-          basePath={basePath}
-          apiDescriptionUrl={apiDescriptionUrl}
-        />
+        <div className="md:px-0 px-4 h-full">
+          <API
+            layout={layout}
+            basePath={basePath}
+            apiDescriptionUrl={apiDescriptionUrl}
+          />
+        </div>
       </div>
     </div>
   )
