2ee453045f44840b087c2722e8e7ee39478a106c
Muhammad Muhajir <muhajirframe@gmail.com>
2023-02-09 19:24:37 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,30 @@
-const withMDX = require('@next/mdx')({
+// next.config.mjs
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx'
+
+const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
+    remarkPlugins: [remarkGfm],
     rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
   },
 })
+
+// import remarkGfm from 'remark-gfm'
+// import createMDX from '@next/mdx'
+
+// const withMDX = createMDX({
+//   extension: /\.mdx?$/,
+//   options: {
+//     // If you use remark-gfm, you'll need to use next.config.mjs
+//     // as the package is ESM only
+//     // https://github.com/remarkjs/remark-gfm#install
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [],
+//     // If you use `MDXProvider`, uncomment the following line.
+//     providerImportSource: '@mdx-js/react',
+//   },
+// })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -44,4 +59,4 @@ const nextConfig = {
   // },
 }
 
-module.exports = withMDX(nextConfig)
+export default withMDX(nextConfig)
