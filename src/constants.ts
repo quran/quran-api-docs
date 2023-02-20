11749b95d047bd0b955154ce275936d47da3c3bd
osamasayed <osamasayed@users.noreply.github.com>
2023-02-20 15:14:58 +0530

    Updates

--- src/constants.ts
+++ src/constants.ts
@@ -1,42 +1,57 @@
-export const APIs = [
-  {
-    label: 'V1 APIs',
-    value: '/qdc-auth-v1',
-    group: 'Quran.com Auth',
-  },
-  {
-    label: 'V4 APIs',
-    value: '/qdc-v4',
-    group: 'Quran.com Backend',
-  },
+enum API_GROUP {
+  AUTH = 'Quran.com Auth APIs',
+  OAUTH2 = 'OAuth 2/OpenId Connect',
+  BACKEND = 'Quran.com Backend APIs',
+}
+
+const oAuth2List = [
   {
     label: 'Getting started With OAuth 2.0',
     value: '/guides/getting-started-with-oauth2',
-    group: 'Quran.com Auth',
-  },
-  {
-    label: 'Scopes',
-    value: '/guides/scopes',
-    group: 'Quran.com Auth',
-  },
-  {
-    label: 'Quran.com',
-    value: '/qdc',
-    group: 'Quran.com Backend',
+    group: API_GROUP.OAUTH2,
   },
   {
     label: 'OAuth2 Tutorial',
     value: '/guides/oauth2-tutorial',
-    group: 'Quran.com Auth',
+    group: API_GROUP.OAUTH2,
   },
-  // {
-  //   label: 'Quran.com OAuth2 Server',
-  //   value: '/qdc-oauth2',
-  //   group: 'Quran.com Auth',
-  // },
+];
+
+const backendList = [
+  {
+    label: 'V4 APIs',
+    value: '/qdc-v4',
+    group: API_GROUP.BACKEND,
+  },
+  {
+    label: 'Quran.com',
+    value: '/qdc',
+    group: API_GROUP.BACKEND,
+  },
 ]
 
-const groups = ['Quran.com Auth', 'Quran.com Backend']
+const authList = [
+  {
+    label: 'V1 APIs',
+    value: '/qdc-auth-v1',
+    group: API_GROUP.AUTH,
+  },
+
+
+  {
+    label: 'Scopes',
+    value: '/guides/scopes',
+    group: API_GROUP.AUTH,
+  },
+]
+
+export const APIs = [
+  ...authList,
+  ...backendList,
+  ...oAuth2List
+]
+
+const groups = [API_GROUP.AUTH, API_GROUP.BACKEND, API_GROUP.OAUTH2]
 
 export const APIGroups = groups.map((group) => ({
   label: group,
