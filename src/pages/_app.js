41fbbb9962b2dbe817d0a4ff3d6d4fb8e7ab495d
osamasayed <osamasayed@users.noreply.github.com>
2023-02-23 10:56:51 +0400

    Add GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,16 @@ import '../styles/global.css'
 import 'highlight.js/styles/a11y-dark.css'
 import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from '../components/MDXComponents'
+import ThirdPartyScripts from '../components/ThirdPartyScripts/ThirdPartyScripts'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
-    <MDXProvider components={MDXComponents}>
-      <Component {...pageProps} />
-    </MDXProvider>
+    <>
+      <MDXProvider components={MDXComponents}>
+        <Component {...pageProps} />
+      </MDXProvider>
+      <ThirdPartyScripts />
+    </>
   )
 }
